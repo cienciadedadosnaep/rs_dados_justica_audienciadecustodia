5eb666e5510a6ac274d5d590a41b448698b1ab18
brunaclimaco <bvictoriaclimaco@gmail.com>
2022-04-04 12:04:46 -0300

    Criação do código "codigo_v00.R" do subtema "Audiência de custódia"

--- codigo_v00.R
+++ codigo_v00.R
@@ -46,61 +46,16 @@ library(jsonlite)
 # #dbClearResult(rs)
 
 library(readr)
-dados <- read_delim("data/audiencia_idade.csv", 
+dd <- read_delim("data/audiencia_idade.csv", 
                               delim = ";", escape_double = FALSE, trim_ws = TRUE)
-names(dados)
-
-# Selecao de parte do banco que responde as perguntas da planilha de povoamento
-
-
+names(dd) = c("ano", "18-29","30-41","42-59","60+")
+dados<- dd %>% select(ano,`18-29`,`30-41`,`42-59`,`60+`)
+nomes <- names(dados)
 ##  Perguntas e titulos 
-
 T_ST_P_No_Justica <- read_csv("data/TEMA_SUBTEMA_P_No - JUSTICA.csv")
 
-# Lembrar de substituir nomes de 
-#names(dados) = c("ano","q1","q2","q3","q41","q42",
-#                 "q43","q44","q45","q46","q47","q48")
 
-names(dados) = c("ano","18-29","30-41","42-59","60+")
-
-
-
-#dados %<>% gather(key = classe,
-#                  value = consumo,-ano) 
-dados_fe <- dados %>% select(ano,`18-29`,`30-41`,`42-59`,`60+`) %>% arrange(ano)
-dados_fe_t <- t(dados_fe)
-
-dados_fe_tn <- data.frame(as.character(row.names(dados_fe_t)),dados_fe_t)
-
-row.names(dados_fe_tn) <- NULL
-
-dados_fe_t_anos <- dados_fe_tn[1,]
-names(dados_fe_t_anos) <- NULL 
-dados_fe_t_anos <- as.character(dados_fe_t_anos)
-
-dados_fe_tl <-  dados_fe_tn[-c(1),]
-
-teste_fe <- list(dados_fe_t_anos,dados_fe_tl)
-
-testejson_fe <- jsonlite::toJSON(teste_fe,dataframe = "values") 
-
-teste2_fe <- gsub('\\[\\[','[',testejson_fe)
-teste3_fe <- gsub('\\]\\]\\]',']',teste2_fe)
-teste_fe 
-
-data_serie <- teste3_fe
-data_serie
-#data_serie <- paste('[',teste3,']',sep = '')
-#data_serie_mod <- gsub('\\\"','"',data_serie)
-
-#dados_adulto <- dados %>% filter(classe %in% c('q43','q44','q45','q46'))
-#dados_idoso <- dados %>% filter(classe %in% c('q47','q48'))
-#dados %<>% select(-id)
-
-# Temas Subtemas Perguntas
-
-
-
+# Selecao de parte do banco que responde as perguntas da planilha de povoamento
 ## Arquivo de saida 
 
 SAIDA_POVOAMENTO <- T_ST_P_No_Justica %>% 
@@ -108,67 +63,83 @@ SAIDA_POVOAMENTO <- T_ST_P_No_Justica %>%
 SAIDA_POVOAMENTO <- as.data.frame(SAIDA_POVOAMENTO)
 
 #classes <- NULL
-#classes <- levels(as.factor(dados_ca$classe))
+#classes <- levels(as.factor(dados$classe))
 
 # Cores secundarias paleta pantone -
-corsec_recossa_azul <- c('#175676','#62acd1','#8bc6d2','#20cfef',
-                         '#d62839','#20cfef','#fe4641','#175676',
-                         '#175676','#62acd1','#8bc6d2','#20cfef')
+corsec_recossa_azul <- c('#a094e1','#dc6f6c','#62acd1','#8bc6d2',
+                         '#d62839','#20cfef','#fe4641','#175676')
+# Cor 1 - Roxo; Cor 2, 5, 7 - Vermelho; Cor 3, 4, 6, 8 - Azul
+
+simbolo_linhas <- c('emptyCircle','emptyTriangle','emptySquare',
+                    'emptyDiamond','emptyRoundRect')
 
 #for ( i in 1:length(classes)) {
-dados <- NULL
-dados <- data_serie
 
+objeto_0 <- dados %>%
+  #filter(classe %in% c(classes[1])) %>%
+  select(ano,`18-29`,`30-41`,`42-59`,`60+`) %>% #filter(ano<2019) %>%
+  #arrange(trimestre) %>%
+  mutate(ano = as.character(ano)) %>% list()               
 
-#  objeto_0 <- dados %>% list()
-#    filter(classe %in% c(classes[i])) %>%
-#    select(ano,consumo) %>% filter(ano<2019) %>%
-#    arrange(ano) %>%
-#    mutate(ano = as.character(ano)) %>% list()               
-
-exportJson0 <- toJSON(teste3_fe)
+exportJson0 <- toJSON(objeto_0)
 
 
 titulo<-T_ST_P_No_Justica$TITULO[1]
-subtexto<-"Fonte: "
+subtexto<-"Fonte: Defensoria Pública - Bahia"
 link <- T_ST_P_No_Justica$LINK[1]
 
-
-texto <- paste('{"title":{"text":"',titulo,
-               '","subtext":"',subtexto,
-               '","sublink":"',link,
-               '"},"legend":{"show":true,"top":"bottom"},"tooltip":{},"dataset":{"source":[',data_serie,
-               ']},"xAxis":[{"type":"category","gridIndex":0}],',
-               '"yAxis":[{"gridIndex":0}],',
-               '"series":[{"type":"bar",','"seriesLayoutBy":"row","color":"',corsec_recossa_azul[1],
-               '","showBackground":false,"backgroundStyle":{"color":"rgba(180, 180, 180, 0)}"},',
-               '"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[1],
-               '","borderWidth":2}},',
-               '{"type":"bar",','"seriesLayoutBy":"row","color":"',corsec_recossa_azul[2],
-               '","showBackground":false,"backgroundStyle":{"color":"rgba(180, 180, 180, 0)}"},',
-               '"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[2],
-               '","borderWidth":2}},',
-               '{"type":"bar",','"seriesLayoutBy":"row","color":"',corsec_recossa_azul[3],
-               '","showBackground":false,"backgroundStyle":{"color":"rgba(180, 180, 180, 0)}"},',
-               '"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[3],
-               '","borderWidth":2}},',
-               '{"type":"bar",','"seriesLayoutBy":"row","color":"',corsec_recossa_azul[4],
-               '","showBackground":false,"backgroundStyle":{"color":"rgba(180, 180, 180, 0)}"},',
-               '"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[4],
-               '","borderWidth":2}}',
-               ']','}',sep="")
-
-## OBS - Incluir 
-## Se for necessario coloca mais colunas além das 2 do default, e escolher 
-## uma cor pelo vetor corsec_recossa_azul[i],
-
-#{"type":"bar",','"seriesLayoutBy":"row","color":"',corsec_recossa_azul[3],
-#               '","showBackground":true,"backgroundStyle":{"color":"rgba(180, 180, 180, 0)}"},',
-#               '"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[3],
-#               '","borderWidth":2}},',
+data_axis <- paste('["',gsub(' ','","',
+                             paste(paste(as.vector(objeto_0[[1]]$ano)),
+                                   collapse = ' ')),'"]',sep = '')
 
 
-#  SAIDA_POVOAMENTO$CODIGO[i] <- texto   
+data_serie <- paste('[',gsub(' ',',',
+                             paste(paste(as.vector(objeto_0[[1]]$`18-29`)),
+                                   collapse = ' ')),']',sep = '')
+
+data_serie2 <- paste('[',gsub(' ',',',
+                              paste(paste(as.vector(objeto_0[[1]]$`30-41`)),
+                                    collapse = ' ')),']',sep = '')
+
+data_serie3 <- paste('[',gsub(' ',',',
+                              paste(paste(as.vector(objeto_0[[1]]$`42-59`)),
+                                    collapse = ' ')),']',sep = '')
+
+data_serie4 <- paste('[',gsub(' ',',',
+                              paste(paste(as.vector(objeto_0[[1]]$`60+`)),
+                                    collapse = ' ')),']',sep = '')
+
+#Colocar o nome da coluna depois de "objeto_0[[1]]$"
+
+texto<-paste('{"title":{"text":"',titulo,
+             '","subtext":"',subtexto,
+             '","sublink":"',link,'"},',
+             '"tooltip":{"trigger":"axis"},',
+             '"toolbox":{"left":"center","orient":"horizontal","itemSize":20,"top":45,"show":true,',
+             '"feature":{"dataZoom":{"yAxisIndex":"none"},',
+             '"dataView":{"readOnly":false},',
+             '"restore":{},"saveAsImage":{}}},"legend":{"show":true,"top":"bottom"},"xAxis":{"type":"category",',
+             '"data":',data_axis,'},',
+             '"yAxis":{"type":"value","axisLabel":{"formatter":"{value}"}},',
+             '"series":[{"name":"',nomes[2],'","data":',data_serie,',',
+             '"type":"bar","color":"',corsec_recossa_azul[6],'","showBackground":true,',
+             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[1],
+             '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[6],'","borderWidth":2}},',
+             '{"name":"',nomes[3],'","data":',data_serie2,',',
+             '"type":"bar","color":"',corsec_recossa_azul[7],'","showBackground":true,',
+             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[2],
+             '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[7],'","borderWidth":2}},',
+             '{"name":"',nomes[4],'","data":',data_serie3,',',
+             '"type":"bar","color":"',corsec_recossa_azul[8],'","showBackground":true,',
+             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[3],
+             '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[8],'","borderWidth":2}},',
+             '{"name":"',nomes[5],'","data":',data_serie4,',',
+             '"type":"bar","color":"',corsec_recossa_azul[1],'","showBackground":true,',
+             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[4],
+             '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[1],'","borderWidth":2}},',
+             ']}',sep='')
+
+#SAIDA_POVOAMENTO$CODIGO[i] <- texto   
 texto<-noquote(texto)
 
 
